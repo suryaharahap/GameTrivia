4baa6f6dc4276f81a6eefc2638fcf135cf5ca9c2
suryaharahap <suryaharahap18@gmail.com>
2021-04-25 21:29:22 +0700

    built the question screen

---
+++ GameTrivia/GameTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTriviaApp.swift
+//  GameTrivia
+//
+//  Created by Surya on 20/04/21.
+//
+
+import SwiftUI
+
+@main
+struct GameTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
